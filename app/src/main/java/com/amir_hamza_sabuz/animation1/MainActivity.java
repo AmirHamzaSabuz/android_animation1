36050a0d9711c1af3993fc23ec639ccbc436ced0
Amir Hamza <hamza_amir@yahoo.com>
2019-06-16 04:45:34 +0600

    Rotate & translation animation

--- app/src/main/java/com/amir_hamza_sabuz/animation1/MainActivity.java
+++ app/src/main/java/com/amir_hamza_sabuz/animation1/MainActivity.java
@@ -15,12 +15,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ivBart = findViewById(R.id.ivBart);
         ivHomer = findViewById(R.id.ivHomer);
+        ivBart.setTranslationX(-1000f);
+        ivBart.setTranslationY(-1000f);
+
     }
 
     public void fade(View view){
-        ivBart.animate().alpha(0f).setDuration(2000);
-        ivHomer.animate().alpha(1f).setDuration(2000);
-
-
+        //ivBart.animate().alpha(0f).setDuration(2000);
+        //ivHomer.animate().alpha(1f).setDuration(2000);
+        //ivBart.animate().translationXBy(1000f).setDuration(2000);
+        //ivBart.animate().rotation(1800f).setDuration(2000);
+        //ivBart.animate().scaleX(0.5f).scaleY(0.5f).setDuration(2000);
+        ivBart
+                .animate()
+                .translationXBy(1000f)
+                .translationYBy(1000f)
+                .rotationBy(3600)
+                .setDuration(2000);
     }
 }
